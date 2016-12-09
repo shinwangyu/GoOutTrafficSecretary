96614ebe8355b15a1c2b6dc583ba2486339fa2ff
김정수 <nafar@naver.com>
2016-12-09 15:45:08 +0900

    1. XML NavigationView 주석 2. MainActivity 교통 정보 API 함수 주석

--- app/src/main/java/wangdaeji/com/goouttrafficsecretary/MainActivity.java
+++ app/src/main/java/wangdaeji/com/goouttrafficsecretary/MainActivity.java
@@ -49,11 +49,11 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        /*NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);*/
 
         //test request API
-        doRequestRetroAPI();
+//        doRequestRetroAPI();
 
     }
 
